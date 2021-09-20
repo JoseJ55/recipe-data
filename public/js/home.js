6136900e24967577c8ffb492b1822cc4753a216f
Jose Jimenez <josejimenez826@gmail.com>
2021-09-19 23:40:53 -0700

    worked on getting and shoing data for adding ingredients

--- public/js/home.js
+++ public/js/home.js
@@ -1,15 +1,18 @@
+// add section elements.
 const addBtn = document.querySelector("#add");
 const searchBtn = document.querySelector("#search");
-
 const addSection = document.querySelector("#addSection");
 const searchSection = document.querySelector("#searchSection");
 
+// add section 
+const ing = [];
+const addIng = document.querySelector("#addIng");
+const addButton = document.querySelector("#addBtn");
+
+// Add section of home page.
 const changeAdd = () => {
-    // console.log(addBtn)
-    // console.log(searchSection)
     searchSection.classList.add("d-none");
     searchSection.classList.remove("d-flex");
-    
     
     addSection.classList.remove("d-none");
     addSection.classList.add("d-flex");
@@ -23,5 +26,19 @@ const changeSearch = () => {
     searchSection.classList.add("d-flex");
 }
 
+// for adding ingrediantes on the add section.
+const addIngredient = () => {
+    // console.log(addIng.value)
+    if (ing.includes(addIng.value)){
+        console.log("Item already added.")
+    } else {
+        ing.push(addIng.value)
+    }
+    addIng.value = ""
+    console.log(ing)
+}
+
 addBtn.addEventListener("click", changeAdd)
 searchBtn.addEventListener("click", changeSearch)
+
+addButton.addEventListener("click", addIngredient)
